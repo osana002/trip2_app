4ba586a6ed08acda9f94c283820b83f58ee6ff48
osana002 <sana.60301@gmail.com>
2017-11-14 07:29:54 +0000

    edit html

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :spot_name, presence: true
   validates :spot_address, presence: true
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -66,4 +66,14 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
+  
+   test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(spot_name: "Example name",
+                             spot_address: "Example address",
+                             content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
