f39153a22d57797b7b5e82439f0312fc8bbc9b07
osana002 <sana.60301@gmail.com>
2017-11-20 07:45:35 +0000

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,11 @@ users = User.order(:created_at).take(6)
                                               spot_address: spot_address,
                                               content: content) }
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
