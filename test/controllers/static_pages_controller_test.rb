b8ba7ecd5a0274a8f061d6867dc23ccc65838e81
osana002 <sana.60301@gmail.com>
2017-12-19 02:03:20 +0000

    heroku push

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,18 +11,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Help | POSTRIP"
+    assert_select "title", "ヘルプ | POSTRIP"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | POSTRIP"
+    assert_select "title", "概要 | POSTRIP"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | POSTRIP"
+    assert_select "title", "お問い合わせ | POSTRIP"
   end
 end
